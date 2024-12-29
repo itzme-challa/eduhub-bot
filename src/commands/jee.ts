9ce5f8a5efdde0772abe4f6c4d0e6f34e98bf0d8
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-12-29 13:52:21 +0530

    Update jee.ts

--- src/commands/jee.ts
+++ src/commands/jee.ts
@@ -1,32 +1,30 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
 
-import { name } from '../../package.json';
+import { name } from '../../package.json'; // Assuming 'name' is already imported from package.json
 
 const debug = createDebug('bot:jee_command');
 
-// Function to send a generic response with a message
-const genericResponse = async (ctx: Context, message: string) => {
-  await ctx.reply(message);
-};
-
-// The JEE command to send resources and available commands
 const jee = () => async (ctx: Context) => {
   const message = `*${name} JEE Resources*:
+  
   \n\n
-  1. For JEE quizzes, use the command: /quizzes
-  2. Join Test Series from the following providers:
-     - [Akash Test Series](https://test-series.pages.dev/)
-     - [Allen Test Series](https://test-series.pages.dev/)
-     - [PW Test Series](https://test-series.pages.dev/)
-     - [Modules and More](https://test-series.pages.dev/)
-  3. Join JEE study groups: /groups
-  4. To list all available commands, use: /list`;
+  **For JEE quizzes**, use the command: \`/quizzes\`
+  
+  **Join Test Series** from the following links:
+  1. Akash Test Series - [Link](https://test-series.pages.dev)
+  2. Allen Test Series - [Link](https://test-series.pages.dev)
+  3. PW Test Series - [Link](https://test-series.pages.dev)
+  4. Modules and More - [Link](https://test-series.pages.dev)
+
+  **JEE Study Groups**: Use the command: \`/groups\`
+  
+  **To list all available commands**, use: \`/list\`
+  `;
 
   debug(`Triggered "jee" command with message \n${message}`);
-
-  // Send the message with available options
-  await genericResponse(ctx, message);
+  
+  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
 };
 
 export { jee };
