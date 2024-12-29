f599b81234fc2437bfbb0c1c9234db28a221b1f4
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-12-29 13:53:44 +0530

    Update quizes.ts

--- src/commands/quizes.ts
+++ src/commands/quizes.ts
@@ -1,24 +1,24 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
 
-import { name } from '../../package.json';
+import { name } from '../../package.json'; // Assuming 'name' is already imported from package.json
 
 const debug = createDebug('bot:quizes_command');
 
 const quizes = () => async (ctx: Context) => {
-  const message = `*${name} Quizes*:
-  \n\n
-  Play quizes for various exams and tests here: [Play Quizes](https://itzfew.github.io/Quizes/)
+  const message = `*${name} Quizzes*:
+  
+  Play quizzes for various exams and tests here:
+  [Play Quizzes](https://itzfew.github.io/Quizes/)
 
-  Available Exams:
+  **Available Exams**:
   - JEE Main Misc
   - JEE Main
   - NEET
   - BITSAT`;
 
-  debug(`Triggered "quizes" command with message \n${message}`);
+  debug(`Triggered "quizes" command with message: \n${message}`);
 
-  // Corrected to use only 'replyWithMarkdownV2'
   await ctx.replyWithMarkdownV2(message);
 };
 
