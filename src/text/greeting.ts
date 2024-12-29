925d3007eb0badbbd796a5edcd07adb8bb781bf0
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-12-29 15:57:26 +0530

    Update greeting.ts

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -3,11 +3,13 @@ import createDebug from 'debug';
 
 const debug = createDebug('bot:greeting_text');
 
+// Function to reply to a specific message
 const replyToMessage = (ctx: Context, messageId: number, string: string) =>
   ctx.reply(string, {
     reply_parameters: { message_id: messageId },
   });
 
+// Function to get a random greeting
 const getRandomGreeting = (): string => {
   const greetings = [
     "Hello dear, {userName}!",
@@ -21,12 +23,13 @@ const getRandomGreeting = (): string => {
   return greetings[randomIndex];
 };
 
+// Main greeting function
 const greeting = () => async (ctx: Context) => {
   debug('Triggered "greeting" text command');
 
   const messageId = ctx.message?.message_id;
   const userName = `${ctx.message?.from.first_name}`;
-  const userMessage = ctx.message?.text?.toLowerCase();
+  const userMessage = ctx.message?.text?.toLowerCase(); // Safely access text property
 
   if (messageId) {
     if (userMessage === '/start') {
