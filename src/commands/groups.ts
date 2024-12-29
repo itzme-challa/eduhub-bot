10dd6e8a3c3212ea787aebb3abad33bc8f69765d
itzme-challa <challawaheed4@gmail.com>
2024-12-29 09:05:02 +0530

    Update groups.ts

--- src/commands/groups.ts
+++ src/commands/groups.ts
@@ -12,19 +12,22 @@ const groups = () => async (ctx: Context) => {
   Following is a list of study groups:
 
   1. NEET Study Group
-  [Link](https://example.com/neet-study-group)
+  [Link](https://t.me/Neetpw01)
 
-  2. JEE Preparation Group
-  [Link](https://example.com/jee-preparation-group)
+  2. FREE STUDY App Group
+  [Link](https://t.me/FREESTUDYApp)
 
-  3. Medical Entrance Exam Group
-  [Link](https://example.com/med-entrance-group)
+  3. Allen Test Series 2024 Group
+  [Link](https://t.me/AllenTest_series2024)
 
-  4. Physics Enthusiasts Group
-  [Link](https://example.com/physics-enthusiasts-group)
+  4. NEET JEE Channel
+  [Link](https://t.me/NEETJEECHANNEL)
 
-  5. Biology Discussion Group
-  [Link](https://example.com/biology-discussion-group)`;
+  5. Akash Test Series Group
+  [Link](https://t.me/AkashTest_Series)
+
+  6. NEET UG 2025 Group
+  [Link](https://t.me/NEETUG_25)`;
 
   debug(`Triggered "groups" command with message \n${message}`);
 
