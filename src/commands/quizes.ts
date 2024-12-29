fffd67f0f1a5a2819b6dd9ab1fc071b9041a83aa
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-12-29 15:25:00 +0530

    Update quizes.ts

--- src/commands/quizes.ts
+++ src/commands/quizes.ts
@@ -8,19 +8,29 @@ const debug = createDebug('bot:quizes_command');
 const quizes = () => async (ctx: Context) => {
   const message = `*${name} Quizzes*:
   
-  Play quizzes for various exams and tests here:
-  [Play Quizzes](https://itzfew.github.io/Quizes/)
-
+  \n\n
+  **Available Quizzes**: Access them at [Quizzes Link](https://itzfew.github.io/Quizes/)
+  
   **Available Exams**:
   - JEE Main Misc
   - JEE Main
   - NEET
-  - BITSAT`;
+  - BITSAT
+  
+  **Join Test Series** from the following links:
+  1. Akash Test Series - [Link](https://test-series.pages.dev)
+  2. Allen Test Series - [Link](https://test-series.pages.dev)
+  3. PW Test Series - [Link](https://test-series.pages.dev)
+  4. Modules and More - [Link](https://test-series.pages.dev)
 
-  debug(`Triggered "quizes" command with message: \n${message}`);
+  **NEET Study Groups**: Use the command: \`/groups\`
+  
+  **To list all available commands**, use: \`/list\`
+  `;
 
-  // Ensure the message uses proper MarkdownV2 format and send it
-  await ctx.replyWithMarkdownV2(message);
+  debug(`Triggered "quizes" command with message \n${message}`);
+  
+  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
 };
 
 export { quizes };
