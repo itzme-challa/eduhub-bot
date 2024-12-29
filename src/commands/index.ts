632266744da09a91e95c0f3afda5b91ac7d909a6
itzme-challa <challawaheed4@gmail.com>
2024-12-29 09:33:35 +0530

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,7 @@ export * from './about';
 export * from './help';
 export * from './study';
 export * from './neet';
+export * from './jee';
 export * from './quizes';
 export * from './groups';
 export * from './list';
