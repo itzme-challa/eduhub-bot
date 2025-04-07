47f9253d6ae7a042348e9b569cf81d46a5df44ba
itzme-challa <challawaheed4@gmail.com>
2025-04-07 18:56:28 +0530

    Update jee.ts

--- src/commands/jee.ts
+++ src/commands/jee.ts
@@ -5,27 +5,32 @@ import { name } from '../../package.json';
 
 const debug = createDebug('bot:jee_command');
 
-// Function to send a generic response with a message
+// Function to send a markdown-formatted message
 const genericResponse = async (ctx: Context, message: string) => {
-  await ctx.reply(message);
+  await ctx.replyWithMarkdownV2(message);
 };
 
-// The JEE command to send resources and available commands
+// The jee command to send resources and available commands
 const jee = () => async (ctx: Context) => {
-  const message = `*${name} JEE Resources*:
-  \n\n
-  1. For JEE quizzes, use the command: /quizzes
-  2. Join Test Series from the following providers:
-     - [Akash Test Series](https://test-series.pages.dev/)
-     - [Allen Test Series](https://test-series.pages.dev/)
-     - [PW Test Series](https://test-series.pages.dev/)
-     - [Modules and More](https://test-series.pages.dev/)
-  3. Join JEE study groups: /groups
-  4. To list all available commands, use: /list`;
+  const message = 
+`*${name} jee Resources*
+
+1\\. For jee quizzes, use the command: \`/quizzes\`
+
+2\\. Join Test Series from:
+  • Akash
+  • Allen
+  • Physics Wallah PW
+  • Modules and more  
+  [Access Test Series Here](https://test-series.pages.dev/)
+
+3\\. Join jee study groups: \`/groups\`
+
+4\\. To list all available commands: \`/list\`
+`;
 
   debug(`Triggered "jee" command with message \n${message}`);
 
-  // Send the message with available options
   await genericResponse(ctx, message);
 };
 
