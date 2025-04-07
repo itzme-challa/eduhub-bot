ea2392298cdc0fb249be4e7eacdff7b1fb194ca2
itzme-challa <challawaheed4@gmail.com>
2025-04-07 18:28:07 +0530

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,25 +6,22 @@ import { name } from '../../package.json';  // Assuming 'name' is already import
 const debug = createDebug('bot:help_command');
 
 const help = () => async (ctx: Context) => {
-  const message = `*${name} Help*:
-  \n\n
-  Here are the available commands you can use:
-  \n
-  /about - Get information about the bot
-  \n
-  /start - Start interacting with the bot
-  \n
-  /help - Show this help message
-  \n\n
-  For more support or inquiries, you can contact us at:
-  \n
-  Email: itzme.eduhub.contact@gmail.com
-  \n
-  Telegram: [@itzfew](https://t.me/itzfew)`;
+  const message = `*${name} Help*\n
+Here are the available commands you can use:\n
+• /help \\- Get information about bot commands  
+• /about \\- Learn more about this bot  
+• /groups \\- Get a list of study groups  
+• /neet \\- Access resources for NEET  
+• /jee \\- Access resources for JEE  
+• /study \\- Get study materials for various subjects  
+
+For more support or inquiries, you can contact us at:\n
+Email: itzme\\.eduhub\\.contact@gmail\\.com  
+Telegram: [@itzfew](https://t.me/itzfew)`;
 
   debug(`Triggered "help" command with message \n${message}`);
   
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+  await ctx.replyWithMarkdownV2(message, { parse_mode: 'MarkdownV2' });
 };
 
 export { help };
