0c3149f55db906b071e2fe1e7ac40ba1e2000296
itzme-challa <challawaheed4@gmail.com>
2025-04-07 18:55:33 +0530

    Update neet.ts

--- src/commands/neet.ts
+++ src/commands/neet.ts
@@ -5,27 +5,32 @@ import { name } from '../../package.json';
 
 const debug = createDebug('bot:neet_command');
 
-// Function to send a generic response with a message
+// Function to send a markdown-formatted message
 const genericResponse = async (ctx: Context, message: string) => {
-  await ctx.reply(message);
+  await ctx.replyWithMarkdownV2(message);
 };
 
 // The NEET command to send resources and available commands
 const neet = () => async (ctx: Context) => {
-  const message = `*${name} NEET Resources*:
-  \n\n
-  1. For NEET quizzes, use the command: /quizzes
-  2. Join Test Series from the following providers:
-    - Akash Test Series: [Link](https://test-series.pages.dev/)
-    - Allen Test Series: [Link](https://test-series.pages.dev/)
-    - PW Test Series: [Link](https://test-series.pages.dev/)
-    - Modules and More: [Link](https://test-series.pages.dev/)
-  3. Join NEET study groups: /groups
-  4. To list all available commands, use: /list`;
+  const message = 
+`*${name} NEET Resources*
+
+1\\. For NEET quizzes, use the command: \`/quizzes\`
+
+2\\. Join Test Series from:
+  • Akash
+  • Allen
+  • Physics Wallah
+  • Modules and more  
+  [Access Test Series Here](https://test-series.pages.dev/)
+
+3\\. Join NEET study groups: \`/groups\`
+
+4\\. To list all available commands: \`/list\`
+`;
 
   debug(`Triggered "neet" command with message \n${message}`);
 
-  // Send the message with available options
   await genericResponse(ctx, message);
 };
 
