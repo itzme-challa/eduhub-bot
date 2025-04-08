5c51ef29377c5ab68db097d03e7194ed94f75c31
itzme-challa <challawaheed4@gmail.com>
2025-04-08 14:05:54 +0530

    Create chatStore.ts

---
+++ src/utils/chatStore.ts
@@ -0,0 +1,13 @@
+// src/utils/chatStore.ts
+
+let chatIds: number[] = [];
+
+export const saveChatId = (id: number) => {
+  if (!chatIds.includes(id)) {
+    chatIds.push(id);
+  }
+};
+
+export const getAllChatIds = (): number[] => {
+  return chatIds;
+};
